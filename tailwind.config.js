eef6a5618f77b3f0e0ee064e992850560b8d1c6c
Amogh Suman <34958121+amoghsuman@users.noreply.github.com>
2026-01-25 21:49:47 +0530

    Add Tailwind CSS configuration file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,11 @@
+export default {
+  content: [
+    "./index.html",
+    "./**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
 // Using Tailwind via CDN in index.html
